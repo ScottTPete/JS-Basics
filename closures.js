b2778755562ab4ede5b38a886ada53407533248c
Scott Peterson <stpeterson28@yahoo.com>
2016-02-05 19:28:05 -0700

    done with all but the last problem in closures

--- closures.js
+++ closures.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+inner();
 
 
 
@@ -32,7 +32,9 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+var call = callFriend();
+
+call('435-215-9248');
 
 
 
@@ -44,7 +46,14 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+  function makeCounter() {
+    var count = 0;
+    function add() {
+      count++;
+      return count;
+    }
+    return add;
+  }
   var count = makeCounter();
   count(); // 1
   count(); // 2
@@ -61,9 +70,44 @@ var callFriend = function(){
   Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
 */
 
-  //Code Here
+/*function codeLove() {
+  return "I love code";
+}
 
+function codeFriend(fn) {
+  var counter = 1;
+  return function() {
+    if(counter === 1) {
+      return fn();
+      counter++;
+    }
+    else {
+      return null;
+    }
+  }
 
+}
+
+var codeEcho = codeFriend(codeLove);*/
+
+function codeLove () {
+ return "I love code";
+
+}
+function codeFriend(fn) {
+ var counter = 1;
+ return function() {
+   if(counter === 1) {
+     counter++;
+     return fn();
+   }else {
+     return null;
+   }
+
+ }
+
+}
+var codeEcho = codeFriend(codeLove);
 
 //Next Problem
 
@@ -73,7 +117,19 @@ var callFriend = function(){
   Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
 */
 
+function fnCounter(fn, N) {
+  var counter = 1;
+  return function() {
+    if(counter <= N) {
+      counter++;
+      return fn();
 
+    }
+    else {
+      return "STOP";
+    }
+  }
+}
 
 //Next Problem
 
@@ -82,28 +138,35 @@ var callFriend = function(){
 /*
   var counter = function(){
     for (var i=1; i<=5; i++) {
-      setTimeout( function timer(){
+      setTimeout( function timer(i){
           console.log( i );
-      }, i*1000 );
+      }, i*1000, i );
     }
   };
 
   Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
-    //Answer Here
+    //It's going to log the value of i after a set amount of time.
 
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
-    //Answer Here
+    //It was right, it logged the value of i after 1 second intervals.
 
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one because of the timeout)
 */
 
-    //Code Here
+var counter = function(){
+    for (var i=1; i<=5; i++) {
+      setTimeout(function timer(i){
+          console.log(i);
+      }, i*1000, i );
+    }
+  };
+  counter();
 
 
 
@@ -123,5 +186,11 @@ var callFriend = function(){
 
   *Hint: Don't let this fool you. Break down what's really happening here.
 */
-
-
+var funcArray = [
+  function() {
+    for(var i = 0; i <= funcArray.length; i++) {
+      funcArray.push(i);
+      return funcArray;
+    }
+  }
+]
